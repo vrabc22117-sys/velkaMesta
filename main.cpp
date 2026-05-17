e6c6a761fc656677d1580158269bc20b70919650
hlave22099-prog <hlave22099@zak.gvid.cz>
2026-05-17 12:06:00 +0200

    Pridan main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdbool.h>
+#include <string.h>
+#define MAX 100
+typedef struct
+{
+    char nazev[40];
+    int pocetOb;
+    char kraj[40];
+    float rozloha;
+} Tmesto;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
